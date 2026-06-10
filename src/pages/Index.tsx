a8a33aacec392efe5abfc21fc756f486c319fa52
Suthesh-004 <sutheshpravin4@gmail.com>
2026-06-10 15:36:02 +0530

    chore : add components to route

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,24 @@
 import Header from "@/components/Header.tsx";
 import HeroSection from "@/components/HeroSection";
 import MapSection from "@/components/MapSection";
+import ServicesGallery from "@/components/ServicesGallery";
+import StatsSection from "@/components/StatsSection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+import Footer from "@/components/Footer";
+import AboutSection from "@/components/AboutSection";
+import ContactSection from "@/components/ContactSection";
 const Index = () => {
   return (
     <div className="min-h-screen relative">
       <Header></Header>
       <HeroSection></HeroSection>
+      <StatsSection></StatsSection>
       <MapSection></MapSection>
+      <ServicesGallery></ServicesGallery>
+      <TestimonialsSection></TestimonialsSection>
+      <ContactSection></ContactSection>
+      <AboutSection></AboutSection>
+      <Footer></Footer>
     </div>
   );
 };
